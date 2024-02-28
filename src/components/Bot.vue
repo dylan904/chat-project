b6aa38b2facc3a60f3e2fca9c187c8e2ace27911
Dylan Maxey <dmaxey@beeline.com>
2024-02-28 17:34:39 -0500

    added bot component

---
+++ src/components/Bot.vue
@@ -0,0 +1,35 @@
+<template>
+    <ChatBox title="Beeline Assistant" />
+  </template>
+    
+    <script>
+    import ChatBox from "../components/ChatBox.vue";
+    
+    export default {
+      name: "ChatBot",
+      components: {
+        ChatBox,
+      },
+      data() {
+        return {};
+      },
+    };
+    </script>
+    
+    <style>
+    *,
+    *::before,
+    *::after {
+      box-sizing: border-box;
+    }
+    
+    #app {
+      font-family: Roboto, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 40px;
+    }
+    </style>
+      
